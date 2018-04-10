b8dd094087770a23ba34636b5207298da10c71cd
Riya <riyakapuria.1992@gmail.com>
2018-04-10 08:49:01 +0530

    in chat.js variable add

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -16,18 +16,33 @@ class Chats extends Component {
     let chat_details =
     (this.props.chatReducer && this.props.chatReducer.chat_details) ?
     this.props.chatReducer.chat_details.users[0].contacts : [];
+    let friends = this.props.chatReducer.chat_details.users;
     let conversation_details =
     (this.props.chatReducer && this.props.chatReducer.conversation_details) ?
     this.props.chatReducer.conversation_details.messages : [];
+    console.log("??????????",chat_details);
     return (
       <div>
         { chat_details.length <= 0 ?
           <PageLoading />
           :
           <div>
+            {friends.map((values,key) => {
+              return(
+                <div key={key}>
+                  {values.id === chat_details ?
+                    <div>{values.firstname} {values.lastname}</div>
+                    : <div>{values.id}</div>
+                  }
+                </div>
+                : null
+              )
+            })}
+            <div>
             {chat_details.map((values,key) => {
               return(
                 <div key={key}>
+
                   <p>Friend: {values}</p>
                     {conversation_details.map((val,k) => {
                       return(
@@ -37,9 +52,10 @@ class Chats extends Component {
                 </div>
               )
             })}
+            </div>
           </div>
-         }
-       </div>
+          }
+        </div>
     );
   }
 }
